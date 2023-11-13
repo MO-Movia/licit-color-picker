f6a5edd4d7657918078913fe5728c60b30af0871
Drishya Das <drishya.das@modusoperandi.com>
2023-11-13 13:30:14 +0530

    Minor UI changes

--- src/Components/ColorEditor.tsx
+++ src/Components/ColorEditor.tsx
@@ -6,32 +6,165 @@ import clamp from "./clamp";
 import "./czi-color-editor.css";
 import ColorPicker, { ColorPickerVariant } from "./ColorPicker";
 
-function generateGreyColors(count: number): Array<Color> {
-  let cc = 255;
-  const interval = cc / count;
-  const colors = [];
-  while (cc > 0) {
-    const color = Color({ r: cc, g: cc, b: cc });
-    cc -= interval;
-    cc = Math.floor(cc);
-    colors.unshift(color);
-  }
-  console.log('colorsgrey', colors)
 
-  return colors;
+const HeaderColors = {
+  "colors": [
+    {
+      "name": "GreyScale",
+      "shades": [
+        { "key": "shade1", "value": "#000000" },
+        { "key": "shade2", "value": "#444444" },
+        { "key": "shade3", "value": "#666666" },
+        { "key": "shade4", "value": "#999999" },
+        { "key": "shade5", "value": "#acacac" },
+        { "key": "shade6", "value": "#c8c8c8" },
+        { "key": "shade7", "value": "#e1e1e1" },
+        { "key": "shade7", "value": "#f3f3f3" },
+        { "key": "shade8", "value": "#ffffff" }
+      ]
+    },
+    {
+      "name": "RandomColors",
+      "shades": [
+        { "key": "shade1", "value": "#ff0000" },
+        { "key": "shade2", "value": "#ffc000" },
+        { "key": "shade3", "value": "#ffff00" },
+        { "key": "shade4", "value": "#92d050" },
+        { "key": "shade4", "value": "#00b050" },
+        { "key": "shade5", "value": "#00b0f0" },
+        { "key": "shade6", "value": "#0070c0" },
+        { "key": "shade7", "value": "#ff00ff" },
+        { "key": "shade8", "value": "#7030a0" }
+      ]
+    }
+  ]
 }
 
-const customColors = [
-  { key: "color1", value: "#E53935" },
-  { key: "color2", value: "#FFB300" },
-  { key: "color3", value: "#FFF176" },
-  { key: "color4", value: "#81C784" },
-  { key: "color5", value: "#2E7D32" },
-  { key: "color6", value: "#03A9F4" },
-  { key: "color7", value: "#008080" },
-  { key: "color8", value: "#ff00ff" },
-  { key: "color9", value: "#512DA8" },
-];
+const customColors = {
+  "colors": [
+    {
+      "name": "Red",
+      "shades": [
+        { "key": "shade1", "value": "#fcf0f0" },
+        { "key": "shade2", "value": "#fadcd9" },
+        { "key": "shade3", "value": "#fabbb4" },
+        { "key": "shade4", "value": "#fc9086" },
+        { "key": "shade5", "value": "#fa5343" },
+        { "key": "shade6", "value": "#d91f11" },
+        { "key": "shade7", "value": "#a1160a" },
+        { "key": "shade8", "value": "#75160c" }
+      ]
+    },
+    {
+      "name": "lightYellow",
+      "shades": [
+        { "key": "shade1", "value": "#fcf2eb" },
+        { "key": "shade2", "value": "#fcddc7" },
+        { "key": "shade3", "value": "#fcbc97" },
+        { "key": "shade4", "value": "#fc9162" },
+        { "key": "shade5", "value": "#e86427" },
+        { "key": "shade6", "value": "#bf4815" },
+        { "key": "shade7", "value": "#8f3415" },
+        { "key": "shade8", "value": "#632b17" }
+      ]
+    },
+    {
+      "name": "Yellow",
+      "shades": [
+        { "key": "shade1", "value": "#faf6cf" },
+        { "key": "shade2", "value": "#f7e379" },
+        { "key": "shade3", "value": "#f5c518" },
+        { "key": "shade4", "value": "#d9a514" },
+        { "key": "shade5", "value": "#b3870e" },
+        { "key": "shade6", "value": "#946613" },
+        { "key": "shade7", "value": "#70491c" },
+        { "key": "shade8", "value": "#54341f" }
+      ]
+    },
+    {
+      "name": "lightGreen",
+      "shades": [
+        { "key": "shade1", "value": "#ebf7da" },
+        { "key": "shade2", "value": "#d5f0b1" },
+        { "key": "shade3", "value": "#aad971" },
+        { "key": "shade4", "value": "#78bf39" },
+        { "key": "shade5", "value": "#52a31d" },
+        { "key": "shade6", "value": "#3c7d0e" },
+        { "key": "shade7", "value": "#2e5c0e" },
+        { "key": "shade8", "value": "#254211" }
+      ]
+    },
+    {
+      "name": "Green",
+      "shades": [
+        { "key": "shade1", "value": "#ebf7ed" },
+        { "key": "shade2", "value": "#c7ebd1" },
+        { "key": "shade3", "value": "#88dba8" },
+        { "key": "shade4", "value": "#43c478" },
+        { "key": "shade5", "value": "#16a163" },
+        { "key": "shade6", "value": "#077d55" },
+        { "key": "shade7", "value": "#075e45" },
+        { "key": "shade8", "value": "#094536" }
+      ]
+    },
+    {
+      "name": "lightBlue",
+      "shades": [
+        { "key": "shade1", "value": "#ebf3f7" },
+        { "key": "shade2", "value": "#c9e7f5" },
+        { "key": "shade3", "value": "#8bd3f7" },
+        { "key": "shade4", "value": "#48b8f0" },
+        { "key": "shade5", "value": "#1195d6" },
+        { "key": "shade6", "value": "#0073ba" },
+        { "key": "shade7", "value": "#08548a" },
+        { "key": "shade8", "value": "#0e3d66" }
+      ]
+    },
+    {
+      "name": "Blue",
+      "shades": [
+        { "key": "shade1", "value": "#f0f4fa" },
+        { "key": "shade2", "value": "#d5e4fa" },
+        { "key": "shade3", "value": "#adccf7" },
+        { "key": "shade4", "value": "#75b1ff" },
+        { "key": "shade5", "value": "#3d8df5" },
+        { "key": "shade6", "value": "#186ade" },
+        { "key": "shade7", "value": "#0d4ea6" },
+        { "key": "shade8", "value": "#103a75" }
+      ]
+    },
+    {
+      "name": "Pink",
+      "shades": [
+        { "key": "shade1", "value": "#fcf0f8" },
+        { "key": "shade2", "value": "#f7daed" },
+        { "key": "shade3", "value": "#f7b7e2" },
+        { "key": "shade4", "value": "#fa87d4" },
+        { "key": "shade5", "value": "#ed4cb7" },
+        { "key": "shade6", "value": "#cc1d92" },
+        { "key": "shade7", "value": "#961574" },
+        { "key": "shade8", "value": "#6b155a" }
+      ]
+    },
+    {
+      "name": "Purple",
+      "shades": [
+        { "key": "shade1", "value": "#f7f2fc" },
+        { "key": "shade2", "value": "#eadcfc" },
+        { "key": "shade3", "value": "#dabefa" },
+        { "key": "shade4", "value": "#c89afc" },
+        { "key": "shade5", "value": "#ac71f0" },
+        { "key": "shade6", "value": "#8f49de" },
+        { "key": "shade7", "value": "#6b30ab" },
+        { "key": "shade8", "value": "#4c277d" }
+      ]
+    }
+  ]
+}
+
+const colorNames = customColors.colors.map(color => color.name);
+const headerColorNames = HeaderColors.colors.map(color => color.name);
+
 
 class ColorEditor extends React.PureComponent<any, any>{
   props: {
@@ -39,170 +172,78 @@ class ColorEditor extends React.PureComponent<any, any>{
     hex?: string;
   };
 
+  renderCustomColors(colorName, arrList, TypeName) {
+    const colorData = arrList.colors.find((color) => color.name === colorName);
+    if (!colorData) {
+      return null;
+    }
+    const colorButtons = colorData.shades.map((shade, shadeIndex) => {
+      const style = {
+        backgroundColor: shade.value,
+        border: shade.value === "#ffffff" ? "1px solid #eeeff1" : "none"
+      };
 
-  renderCustomColor() {
-    const customColorElements = customColors.map((colorData, index) => {
-      const style = { backgroundColor: colorData.value };
-      return (
-        <button
-          className="czi-color-editor-cell"
-          key={`custom-color-${index}`}
-          onClick={() => this._onSelectColor(colorData.value)}
-          style={style}
-        />
-      );
+      if (TypeName == "Custom") {
+        return (
+          <tr><td
+            key={`custom-color-${shadeIndex}`}
+            onClick={() => this._onSelectColor(shade.value)}
+            style={style}
+            className="czi-color-editor-cell"
+          ></td></tr>
+        );
+      }
+      else {
+        return (
+          <td
+            key={`custom-color-${shadeIndex}`}
+            onClick={() => this._onSelectColor(shade.value)}
+            style={style}
+            className="czi-color-editor-cell"
+          ></td>
+        );
+      }
+
+
+
     });
+    if (TypeName == "Custom") {
+      return (
+        <tr><td className="czi-color-editor-column">
+          {colorButtons}
+        </td></tr>
+      );
+    }
+    else {
+      return (
+        <tr className="czi-color-editor-column">
+          {colorButtons}
+        </tr>
+      );
+    }
 
-    return customColorElements;
   }
 
-
-  renderCustomColorsWith10PercentOpacity() {
-    const customColorElementsWith10PercentOpacity = customColors.map((colorData, index) => {
-      const style = { backgroundColor: `${colorData.value}1A` }; // 1A adds 10% opacity
-      return (
-        <button
-          className="czi-color-editor-cell"
-          key={`custom-color-opacity-10-${index}`}
-          onClick={() => this._onSelectColor(`${colorData.value}`)}
-          style={style}
-        />
-      );
-    });
-
-    return customColorElementsWith10PercentOpacity;
-  }
-  renderCustomColorsWith20PercentOpacity() {
-    const customColorElementsWith20PercentOpacity = customColors.map((colorData, index) => {
-      const style = { backgroundColor: `${colorData.value}33` }; // 33 adds 20% opacity
-      return (
-        <button
-          className="czi-color-editor-cell"
-          key={`custom-color-opacity-20-${index}`}
-          onClick={() => this._onSelectColor(`${colorData.value}`)}
-          style={style}
-        />
-      );
-    });
-
-    return customColorElementsWith20PercentOpacity;
-  }
-
-  renderCustomColorsWith30PercentOpacity() {
-    const customColorElementsWith30PercentOpacity = customColors.map((colorData, index) => {
-      const style = { backgroundColor: `${colorData.value}4D` }; // 4D adds 30% opacity
-      return (
-        <button
-          className="czi-color-editor-cell"
-          key={`custom-color-opacity-30-${index}`}
-          onClick={() => this._onSelectColor(`${colorData.value}`)}
-          style={style}
-        />
-      );
-    });
-
-    return customColorElementsWith30PercentOpacity;
-  }
-
-  renderCustomColorsWith40PercentOpacity() {
-    const customColorElementsWith40PercentOpacity = customColors.map((colorData, index) => {
-      const style = { backgroundColor: `${colorData.value}66` }; // 66 adds 40% opacity
-      return (
-        <button
-          className="czi-color-editor-cell"
-          key={`custom-color-opacity-40-${index}`}
-          onClick={() => this._onSelectColor(`${colorData.value}`)}
-          style={style}
-        />
-      );
-    });
-
-    return customColorElementsWith40PercentOpacity;
-  }
-
-  renderCustomColorsWith50PercentOpacity() {
-    const customColorElementsWith50PercentOpacity = customColors.map((colorData, index) => {
-      const style = { backgroundColor: `${colorData.value}80` }; // 80 adds 50% opacity
-      return (
-        <button
-          className="czi-color-editor-cell"
-          key={`custom-color-opacity-50-${index}`}
-          onClick={() => this._onSelectColor(`${colorData.value}`)}
-          style={style}
-        />
-      );
-    });
-
-    return customColorElementsWith50PercentOpacity;
-  }
-
-  renderCustomColorsWith60PercentOpacity() {
-    const customColorElementsWith60PercentOpacity = customColors.map((colorData, index) => {
-      const style = { backgroundColor: `${colorData.value}99` };
-      return (
-        <button
-          className="czi-color-editor-cell"
-          key={`custom-color-opacity-60-${index}`}
-          onClick={() => this._onSelectColor(`${colorData.value}`)}
-          style={style}
-        />
-      );
-    });
-
-    return customColorElementsWith60PercentOpacity;
-  }
-
-  renderCustomColorsWith70PercentOpacity() {
-    const customColorElementsWith70PercentOpacity = customColors.map((colorData, index) => {
-      const style = { backgroundColor: `${colorData.value}B3` };
-      return (
-        <button
-          className="czi-color-editor-cell"
-          key={`custom-color-opacity-70-${index}`}
-          onClick={() => this._onSelectColor(`${colorData.value}`)}
-          style={style}
-        />
-      );
-    });
-
-    return customColorElementsWith70PercentOpacity;
-  }
-
-  renderCustomColorsWith80PercentOpacity() {
-    const customColorElementsWith80PercentOpacity = customColors.map((colorData, index) => {
-      const style = { backgroundColor: `${colorData.value}CC` };
-      return (
-        <button
-          className="czi-color-editor-cell"
-          key={`custom-color-opacity-80-${index}`}
-          onClick={() => this._onSelectColor(`${colorData.value}`)}
-          style={style}
-        />
-      );
-    });
-
-    return customColorElementsWith80PercentOpacity;
-  }
+  renderCustomColorsTable = () => {
+    return colorNames.map((colorName, index) => (
+      <table key={colorName} className={`${index === 0 ? 'customFirstColorTable' : 'customColorTable'}`}>
+        <tbody>{this.renderCustomColors(colorName, customColors, "Custom")}</tbody>
+      </table>
+    ));
+  };
 
 
-  renderCustomColorsWith90PercentOpacity() {
-    const customColorElementsWith90PercentOpacity = customColors.map((colorData, index) => {
-      const style = { backgroundColor: `${colorData.value}E6` };
-      return (
-        <button
-          className="czi-color-editor-cell"
-          key={`custom-color-opacity-90-${index}`}
-          onClick={() => this._onSelectColor(`${colorData.value}`)}
-          style={style}
-        />
-      );
-    });
-
-    return customColorElementsWith90PercentOpacity;
-  }
-
-
-
+  renderHeaderColorsTable = () => {
+    return headerColorNames.map((colorName) => (
+      <div key={colorName}>
+        <table className="randomColorTable">
+          <tbody>
+            {this.renderCustomColors(colorName, HeaderColors, "Header")}
+          </tbody>
+        </table>
+      </div>
+    ));
+  };
 
   constructor(props) {
     super(props);
@@ -220,7 +261,6 @@ class ColorEditor extends React.PureComponent<any, any>{
     const selectedColor = this.props.hex;
 
     const handleChange = (color) => {
-      console.log("colorr", color);
       this.setState({ color });
     };
 
@@ -232,43 +272,16 @@ class ColorEditor extends React.PureComponent<any, any>{
           <div >
             <div className="czi-color-editor-section">
               <div
-                className="czi-color-editor-color-transparent czi-color-head"
-              >
+                className="czi-color-editor-color-transparent czi-color-head">
                 Select Color
               </div>
             </div>
-            <div className="czi-color-editor-section">
-              {generateGreyColors(10).map(renderColor)}
+            <div>
+              {this.renderHeaderColorsTable()}
             </div>
-            <div className="czi-color-editor-section">
-              {this.renderCustomColor()}
-            </div>
-            <div className="czi-color-editor-section">
-              {this.renderCustomColorsWith10PercentOpacity()}
-            </div>
-            <div className="czi-color-editor-section">
-              {this.renderCustomColorsWith20PercentOpacity()}
-            </div>
-            <div className="czi-color-editor-section">
-              {this.renderCustomColorsWith30PercentOpacity()}
-            </div>
-            <div className="czi-color-editor-section">
-              {this.renderCustomColorsWith40PercentOpacity()}
-            </div>
-            <div className="czi-color-editor-section">
-              {this.renderCustomColorsWith50PercentOpacity()}
-            </div>
-            <div className="czi-color-editor-section">
-              {this.renderCustomColorsWith60PercentOpacity()}
-            </div>
-            <div className="czi-color-editor-section">
-              {this.renderCustomColorsWith70PercentOpacity()}
-            </div>
-            <div className="czi-color-editor-section">
-              {this.renderCustomColorsWith80PercentOpacity()}
-            </div>
-            <div className="czi-color-editor-section">
-              {this.renderCustomColorsWith90PercentOpacity()}
+
+            <div className="flexCustomArea">
+              {this.renderCustomColorsTable()}
             </div>
 
             <hr></hr>
@@ -313,7 +326,6 @@ class ColorEditor extends React.PureComponent<any, any>{
                   Ok
                 </button>
               </div>
-
 
             </div>
 
